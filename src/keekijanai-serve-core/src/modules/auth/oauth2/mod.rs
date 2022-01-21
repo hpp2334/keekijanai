e591cfa40dd7d75f2131e3a344495534208928ef
hpp2334 <hpp2334@outlook.com>
2022-01-22 01:01:29 +0800

    fix(backend/auth): oauth2 rely on setting instead of env

--- src/keekijanai-serve-core/src/modules/auth/oauth2/mod.rs
+++ src/keekijanai-serve-core/src/modules/auth/oauth2/mod.rs
@@ -5,19 +5,6 @@ use self::core::OAuth2Service;
 use crate::{core::setting::SETTING, modules::auth::oauth2::core::OAuth2Config};
 use github::Github;
 
-use std::env;
-
-lazy_static! {
-    static ref GITHUB: Github = {
-        let setting = SETTING.get().unwrap();
-        let setting_github = setting.auth.github.as_ref().unwrap();
-        return Github::new(OAuth2Config {
-            client_id: setting_github.client_id.clone(),
-            client_secret: setting_github.client_secret.clone(),
-        });
-    };
-}
-
 pub struct OAuth2Manager {}
 
 impl OAuth2Manager {
@@ -26,12 +13,16 @@ impl OAuth2Manager {
     }
 
     pub fn get(&self, service: &str) -> anyhow::Result<impl OAuth2Service> {
+        let setting = SETTING.get().unwrap();
         match service {
-            "github" => Ok(Github::new(OAuth2Config {
-                client_id: env::var("GITHUB_CLIENT_ID").expect("GITHUB_CLIENT_ID not in var"),
-                client_secret: env::var("GITHUB_CLIENT_SECRET")
-                    .expect("GITHUB_CLIENT_SECRET not in var"),
-            })),
+            "github" => {
+                let setting_github = setting.auth.github.as_ref().unwrap();
+
+                Ok(Github::new(OAuth2Config {
+                    client_id: setting_github.client_id.clone(),
+                    client_secret: setting_github.client_secret.clone(),
+                }))
+            }
             _ => Err(anyhow::anyhow!("unsupport service")),
         }
     }
