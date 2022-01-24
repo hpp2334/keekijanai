7e4abc1133428d56d7520d71f63ea597a9adda37
hpp2334 <hpp2334@outlook.com>
2022-01-25 00:51:16 +0800

    feat(frontend-toolkit): replace esbuild with babel

--- src/keekijanai-frontend-toolkit/src/build.ts
+++ src/keekijanai-frontend-toolkit/src/build.ts
@@ -1,7 +1,9 @@
 import { program } from "commander";
-import * as esBuild from "esbuild";
 import * as path from "path";
 import * as glob from "glob-promise";
+import * as fs from "fs/promises";
+import { existsSync } from "fs";
+import * as babel from "@babel/core";
 
 interface BuildOptions {
   environment?: "prod" | "dev";
@@ -9,10 +11,104 @@ interface BuildOptions {
   outputDir: string;
 }
 
+interface FileInfo {
+  source: string;
+  absoluteSource: string;
+  absoluteDest: string;
+}
+
+const group = <T>(array: T[], test: (value: T) => boolean) => {
+  const a: T[] = [];
+  const b: T[] = [];
+  array.forEach((item) => (test(item) ? a : b).push(item));
+  return [a, b] as const;
+};
+
+const smartWriteFile = async (filepath: string, buf: string | Buffer) => {
+  const dir = path.dirname(filepath);
+  if (!existsSync(dir)) {
+    await fs.mkdir(dir, { recursive: true });
+  }
+  await fs.writeFile(filepath, buf);
+};
+
+const calcCopyInfo = (sources: string[], destDir: string): Array<FileInfo> => {
+  if (sources.length === 0) {
+    return [];
+  }
+  const splitedSources = sources.map((x) => x.split("/"));
+  let lcaLength = 0;
+  const isCP = (x: string[][], index: number) => {
+    const ps = x.map((x) => (x.length <= index ? null : x[index]));
+    return ps.slice(1).every((x) => ps[0] !== null && x !== null && x === ps[0]);
+  };
+  while (splitedSources.length > 1 && isCP(splitedSources, lcaLength)) {
+    lcaLength += 1;
+  }
+  const destSuffix = splitedSources.map((x) => x.slice(lcaLength).join("/"));
+
+  const info = Array.from({ length: sources.length })
+    .map((_, i) => i)
+    .map((index) => {
+      const baseSource = sources[index];
+      const baseDestSuffix = destSuffix[index];
+      const source = path.resolve(process.cwd(), baseSource);
+      const dest = path.join(path.resolve(process.cwd(), destDir), baseDestSuffix);
+
+      return {
+        source: baseSource,
+        absoluteSource: source,
+        absoluteDest: dest,
+      };
+    });
+  return info;
+};
+
+const build = async (params: { fileInfos: FileInfo[] }) => {
+  await Promise.all(
+    params.fileInfos.map(async (info) => {
+      const res = await babel.transformFileAsync(info.absoluteSource, {
+        presets: [
+          [
+            "@babel/preset-react",
+            {
+              runtime: "automatic",
+            },
+          ],
+          [
+            "@babel/preset-typescript",
+            {
+              isTSX: info.source.endsWith(".tsx"),
+              allExtensions: true,
+            },
+          ],
+        ],
+        plugins: [
+          [
+            require.resolve("babel-plugin-module-resolver"),
+            {
+              root: ["./src"],
+              alias: {
+                "@": "./src",
+              },
+            },
+          ],
+          "babel-plugin-transform-typescript-metadata",
+          ["@babel/plugin-proposal-decorators", { legacy: true }],
+        ],
+      });
+      if (res && res.code !== null && res.code !== undefined) {
+        const dest = info.absoluteDest.replace(/\.ts(x?)$/, ".js$1");
+        await smartWriteFile(dest, res.code);
+      }
+    })
+  );
+};
+
 program
   .command("build")
   .option("-e, --environment <environment>", "environment", "dev")
-  .option("--input <input>", "input glob", "./src/**/*.ts")
+  .option("--input <input>", "input glob", "./src/**/*.+(ts|tsx|json)")
   .option("--outputDir <output>", "output dir", "./dist")
   .action(async function (opts: BuildOptions) {
     const env = opts.environment ?? "dev";
@@ -20,14 +116,20 @@ program
     const outputDir = path.resolve(process.cwd(), opts.outputDir);
 
     const filePaths = await glob.promise(inputGlob);
+    const copyInfo = calcCopyInfo(filePaths, outputDir);
 
-    const commonOpts: esBuild.BuildOptions = {
-      entryPoints: filePaths,
-      outdir: outputDir,
-      bundle: false,
-    };
+    const [toCompileFileInfos, skipCompileFileInfos] = group(copyInfo, (p) => /\.tsx?$/.test(p.source));
 
-    const esBuildOpts = env === "dev" ? { ...commonOpts } : { ...commonOpts, minify: true };
-
-    await esBuild.build(esBuildOpts);
+    await Promise.all([
+      build({
+        fileInfos: toCompileFileInfos,
+      }),
+      skipCompileFileInfos.map(async (info) => {
+        const dir = path.dirname(info.absoluteDest);
+        if (!existsSync(dir)) {
+          await fs.mkdir(dir, { recursive: true });
+        }
+        await fs.copyFile(info.absoluteSource, info.absoluteDest);
+      }),
+    ]);
   });
